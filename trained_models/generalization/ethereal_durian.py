38e97b179399c6ee5ed096e8a692c9b13fbc4875
Carlos Osorio <carlososoriov@gmail.com>
2023-05-06 13:35:47 -0700

    Experiment larger fc layers

--- trained_models/generalization/ethereal_durian.py
+++ trained_models/generalization/ethereal_durian.py
@@ -30,29 +30,29 @@ class DQNSolver(nn.Module):
         self.conv_to_fc = nn.Sequential(
             nn.Linear(conv_out_size, 512),
             nn.LeakyReLU(),
-            nn.Linear(512, 64),
+            nn.Linear(512, 70),
             nn.LeakyReLU()
         )
 
         # We take a vector of 5 being the initial action, and 5 being the second action for action size of 10
         self.actions_fc = nn.Sequential(
             # nn.Linear(self.action_size, 100),
-            nn.Linear(self.action_size, 32),
+            nn.Linear(self.action_size, 30),
             nn.LeakyReLU(),
-            nn.Linear(32, 32),
+            nn.Linear(30, 30),
             nn.LeakyReLU(),
             # nn.ReLU()
         )
         self.fc = nn.Sequential(
             # nn.Linear(conv_out_size + 100, 512),
-            nn.Linear(64 + 32, 32),
+            nn.Linear(70 + 30, 32),
             # nn.BatchNorm1d(32),
             nn.LeakyReLU(),
             # nn.Linear(128, 32), # added a new layer can play with the parameters
             # nn.BatchNorm1d(32),
-            nn.Linear(32, 32), # added a new layer can play with the parameters
+            # nn.Linear(512, 64), # added a new layer can play with the parameters
             # nn.BatchNorm1d(64),
-            nn.LeakyReLU(),
+            # nn.LeakyReLU(),
             # nn.Linear(64, 1)
             nn.Linear(32, 1)
         )
